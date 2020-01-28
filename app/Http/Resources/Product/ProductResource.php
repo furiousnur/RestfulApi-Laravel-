a6ba9f9b29786ef46b50cb30c84785ebc513a6f1
furiousnur <furiousnur@gmail.com>
2020-01-28 23:47:32 +0600

    Product collection transforming

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,16 +15,16 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-          'name' => $this->name,
-          'description' => $this->detail,
-          'price' => $this->price,
-          'stock' => $this->stock== 0 ? 'Out of stock' : $this->stock,
-          'discount' => $this->discount,
-          'totalPrice' => round((1-($this->discount/100)) * $this->price,2),
-          'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
-          'href' =>[
-              'reviews'=> route('reviews.index',$this->id)
-          ]
+            'name' => $this->name,
+            'description' => $this->detail,
+            'price' => $this->price,
+            'stock' => $this->stock== 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round((1-($this->discount/100)) * $this->price,2),
+            'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'href' =>[
+                'reviews'=> route('reviews.index',$this->id)
+            ]
         ];
     }
 }
