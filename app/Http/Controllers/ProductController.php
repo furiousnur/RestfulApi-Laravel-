c96585bbe36b9aaea21ee7369b62b4c246f166bd
furiousnur <furiousnur@gmail.com>
2020-01-29 00:16:58 +0600

    get review api resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return ProductCollection::collection(Product::all());
+        return ProductResource::collection(Product::paginate(3));
     }
 
     /**
